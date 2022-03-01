678bc686855b016a2212646e8e8e7513b2ea4758
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-01 08:46:41 +0000

    update

---
+++ messiGoals.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    let goals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return goals 
+  }
