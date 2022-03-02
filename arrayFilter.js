9708098f08d42d13912b2ca2d7fe2b39d9fc2268
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-02 14:40:51 +0000

    update

---
+++ arrayFilter.js
@@ -0,0 +1,12 @@
+const getEvenNumbers=(numbersArray)=>{
+    // filter out the odd numbers
+    let result = []
+    for (let i = 0 ; i<numbersArray.length ; i++ ){
+    if(numbersArray[i] % 2 === 0){
+      result.push(numbersArray[i])
+    }
+      return results
+    }
+    
+  }
+  
