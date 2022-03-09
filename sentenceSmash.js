f2bfa672c4a7af0d308938ba2aef7bb08147ad5b
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-09 09:20:31 +0000

    update

---
+++ sentenceSmash.js
@@ -0,0 +1,9 @@
+//Join strings into one string 
+
+const smash = (words) => {
+    return words.join( )
+      
+   };
+   
+   smash(["hello", "world"]) //'hello,world'
+   smash(["hello", "amazing", "world"]) //'hello,amazing,world'
