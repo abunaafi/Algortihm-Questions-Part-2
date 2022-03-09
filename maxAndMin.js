fda5c63c6387aec8dc9fe87d87049b772da4d68a
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-09 11:24:01 +0000

    update

--- maxAndMin.js
+++ maxAndMin.js
@@ -19,3 +19,7 @@ const min = (list)=>{
   }
   min([-52, 56, 30, 29, -54, 0, -110]) //-110
   max([4,6,2,1,9,63,-134,566]) // 566
+
+  //below is one of the solutions which was very beautiful and simple 
+  const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
