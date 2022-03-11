ed70f262a257adde58da70bdcf3f4f25eeeaf817
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-11 11:28:28 +0000

    update

---
+++ convertHexToDec.js
@@ -0,0 +1,10 @@
+const hexToDec = (hexString) => {
+    //your code here
+    return parseInt(hexString, 16);
+    
+    
+  }
+  
+  
+  hexToDec("a") // should give 10
+  hexToDec("FF") // should give 255
