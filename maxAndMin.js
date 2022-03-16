28a5e2bc9b2ecb962c195f4293ee6cf635a9c1eb
abunaafi <abdisalanyusuf10@gmail.com>
2022-03-16 14:41:59 +0000

    update

--- maxAndMin.js
+++ maxAndMin.js
@@ -9,7 +9,7 @@ const min = (list)=>{
   }
   
   const max = (list)=>{
-    let result = list [0]
+    let result = list [0]   
     for(let i= 0; i<list.length; i++){
       if (result < list[i]){
         result = list[i]
